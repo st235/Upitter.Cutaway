0769b696997ba40e1460c3e9b382e1723af96a8f
flyingcartman <beyondbirthday90@gmail.com>
2016-09-23 15:46:49 +0300

    Added company social links component

--- scripts/components/companyMain/aside/profileContent/profileContacts/companySocialIcons/companySocialIconsComponent.js
+++ scripts/components/companyMain/aside/profileContent/profileContacts/companySocialIcons/companySocialIconsComponent.js
@@ -1,24 +1,43 @@
 'use strict';
 
 import React from 'react';
+import _ from 'underscore';
+
 import BaseLayout from '../../../../../baseLayout/baseLayout';
 
 class SocialIconsComponent extends BaseLayout {
 	onBind() {
-
+		this.generateSocialLinks = this.generateSocialLinks.bind(this);
 	}
 
 	onCreate() {
 
 	}
 
+	generateSocialLinks(socialArr) {
+		if (!socialArr || !socialArr.length) return null;
+
+		return _.map(socialArr, (network, index) => {
+			if (!network.link || !network.icon) return null;
+
+			const style = {
+				backgroundImage: `url(${network.link})`
+			};
+
+			return (
+				<a key={ index } style={ style } href={ network.link }>
+					<div className="social-icon"></div>
+				</a>
+			);
+		});
+	}
+
 	render() {
+		const { company } = this.store.getState();
+
 		return (
 			<div className="profile-contacts_social">
-				<div className="social-icon social-icon--vk"></div>
-				<div className="social-icon social-icon--facebook"></div>
-				<div className="social-icon social-icon--instagram"></div>
-				<div className="social-icon social-icon--twitter"></div>
+				{ this.generateSocialLinks(company.socialLinks) }
 			</div>
 		);
 	}
@@ -27,3 +46,8 @@ class SocialIconsComponent extends BaseLayout {
 export default SocialIconsComponent;
 
 
+
+// <div className="social-icon social-icon--vk"></div>
+// <div className="social-icon social-icon--facebook"></div>
+// 	<div className="social-icon social-icon--instagram"></div>
+// 	<div className="social-icon social-icon--twitter"></div>
