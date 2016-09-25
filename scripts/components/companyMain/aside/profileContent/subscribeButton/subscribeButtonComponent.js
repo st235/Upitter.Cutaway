8b8347ada4c849008d531264b244149d8ca40cc4
flyingcartman <beyondbirthday90@gmail.com>
2016-09-25 18:40:56 +0300

    Edited subscribe button

--- scripts/components/companyMain/aside/profileContent/subscribeButton/subscribeButtonComponent.js
+++ scripts/components/companyMain/aside/profileContent/subscribeButton/subscribeButtonComponent.js
@@ -5,23 +5,29 @@ import BaseLayout from '../../../../baseLayout/baseLayout';
 
 class SubscribeButtonComponent extends BaseLayout {
 	onBind() {
-
+		this.onSubscribe = this.onSubscribe.bind(this);
 	}
 
 	onCreate() {
 
 	}
 
+	onSubscribe(customId) {
+		return e => {
+			console.log(customId);
+		};
+	}
+
 	render() {
 		const { company } = this.store.getState();
+		const { subscribersAmount, isMySubscription, customId } = company;
 
 		return (
-			//TODO: Доделать число подписчиков
 			//TODO: Что делать, если подписан?
 			<div className="profile-follow">
-				<div className="btn btn--line btn--success btn--counter">
+				<div className="btn btn--line btn--success btn--counter" onClick={ this.onSubscribe(customId) }>
 					{ this.localeService.getLocalizedNameFor('subscribe') }
-					<span>247</span>
+					<span>{ subscribersAmount }</span>
 				</div>
 			</div>
 		);
