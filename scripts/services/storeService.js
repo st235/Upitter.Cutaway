225dfda88e20a6505e845088845baed1584058d4
flyingcartman <beyondbirthday90@gmail.com>
2017-01-13 19:03:51 +0300

    Added redux dev tools

--- scripts/services/storeService.js
+++ scripts/services/storeService.js
@@ -3,7 +3,7 @@ import reducers from '../reducers/indexReducer';
 
 export default class StoreService {
 	static init(renderTree) {
-		this.store = createStore(reducers);
+		this.store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 		this.store.subscribe(renderTree);
 	}
 
